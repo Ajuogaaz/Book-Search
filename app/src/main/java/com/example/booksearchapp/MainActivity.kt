efd479cab50b4cac2c9fbb0fc8ff2f0f8dae63ea
Linus Brian Okoth <okoth@fb.com>
2020-06-27 00:46:56 -0400

    "Update"

--- app/src/main/java/com/example/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.booksearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+"Here"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
